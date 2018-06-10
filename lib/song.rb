bfa6f060296e6ac30f4d2ecf4af314febffefdc2
Justin Kenney <justinkenney13@gmail.com>
2018-06-10 21:52:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,8 +18,8 @@ class Song
   end
 
   def artist_name=(name)
-      self.artist = Artist.find_or_create_by_name(name)
-      self.artist.add_song(self)
+      Artist.find_or_create_by_name(name)
+      Artist.add_song(self)
 
   end
 end
